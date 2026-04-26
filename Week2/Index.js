4147176db62388bf2f7d8b848ceb01d6e41c7b89
chitrapandey8 <chitranshipandey8@gmail.com>
2026-04-26 15:17:58 +0530

    Backend

--- Week2/Index.js
+++ Week2/Index.js
@@ -1,20 +1,32 @@
 import express from  'express'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const app = express()
 
-const PORT = 3000
+const PORT =  process.env.PORT ||  3000
 
 app.use(express.json())
-app.get('', (req, res)=>{
-    res.json({
+
+app.get('/', (req, res)=>{
+    res.status(200).json({
         name:"chitanshi",
         age:20
     })
 })
 
-app.post('/add',(req, res)=>{ 
-    console.log(req.body); //ye client se arra hai server papr mai usse log karri hu
-   res.json({mes:"hello"}) //ye mai client ko send karri hu /add pe hit hone pe
+// app.post('/add',(req, res)=>{ 
+//     console.log(req.body); //ye client se arra hai server papr mai usse log karri hu
+//    res.json({mes:"hello"}) //ye mai client ko send karri hu /add pe hit hone pe
+// })
+
+
+app.post('/add',(req, res)=>{
+    const {a,b} = req.body
+    let sum = parseInt(a)+parseInt(b)
+    res.status(200).json({
+       result:sum
+    })
 })
 
 app.listen(PORT,()=>{
