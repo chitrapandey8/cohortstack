d4759d33f7e4941b3207213fd0d5f8978c01454c
chitrapandey8 <chitranshipandey8@gmail.com>
2026-04-28 16:17:27 +0530

    webdev

--- WebDev/Second.js
+++ WebDev/Second.js
@@ -1,1 +1,17 @@
-console.log("hello im second");
+//console.log("hello im second");
+// export function sum(a, b){
+//     return a+b;
+// }
+
+//i can do this same thing as 
+
+export function sum(a, b){
+    return a+b;
+}
+
+export function sub(a, b){
+    return a-b;
+}
+
+// module.exports  = {sum, sub}; //module.exports ek object hai
+// module.exports.sum = sum -- this is also a way.
--- WebDev/first.js
+++ WebDev/first.js
@@ -1,3 +1,12 @@
-require("./Second.js")
+//running two files at the same time.
+// const {sum, sub} = require("./Second.js") //second wali filw ke code ko ek function mai wrap kar dega. 
+
+import { sub, sum } from "./Second.js";
+
+
+let ans = sum(2,3);
+let subb = sub(5,1);
+console.log(ans);
+console.log(subb);
 
 console.log("hello im first");
