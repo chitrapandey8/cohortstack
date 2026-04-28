1ce74c31858750e24429d18d2f88aebb4e6a9dd6
chitrapandey8 <chitranshipandey8@gmail.com>
2026-04-28 15:53:06 +0530

    push

--- Cohort/Week2/FileAssigment.js
+++ Cohort/Week2/FileAssigment.js
@@ -4,26 +4,17 @@ import { fileURLToPath } from 'url' //library
 import fs from 'fs'
 const app = express()
 const PORT = 3000
-
 const __filename = fileURLToPath(import.meta.url) //current file ka url dega path format mai convert hopga taki os ko samjha aaye.
 const __dirname = path.dirname(__filename) //__filename ki directory name.
 const folderpathofNotes = path.join(__dirname,"notes");
 console.log(folderpathofNotes);
-
 app.use(express.static(folderpathofNotes))
-
 app.get('/getfolders',(req,res)=>{
 fs.readdir(folderpathofNotes, (err, files)=>{
     if(err){
-      res.status(400).json({
-        message:`Wrong Folder Name`
-      })
-    }else{
-      res.status(200).json({files})
-    }
-})
-})
-
+      return res.status(400).json({
+        message:`Wrong Folder Name`})}else{
+      res.status(200).json({files})}})})
 app.get('/readfile/:filename', (req ,res)=>{
 const file = req.params.filename
 const foldername = path.join(folderpathofNotes, file)
@@ -31,15 +22,7 @@ fs.readFile(foldername, "utf-8", (err, data)=>{
     if(err){
         return res.status(400).json({
             message:`Wrong File Name`
-        })
-    }else{
-        return res.status(200).json({data})
-    }
-})
-
-
-})
+        })}else{
+        return res.status(200).json({data})}})})
 app.listen(PORT,()=>{
-    console.log(`Listen on port ${PORT}`);
-    
-})
+    console.log(`Listen on port ${PORT}`);})
---
+++ WebDev/first.js
@@ -0,0 +1,3 @@
+require("./Second.js")
+
+console.log("hello im first");
